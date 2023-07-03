1255af0eb91fec88efb00fda7679beb2effed733
Luis <luisfroquez@gmail.com>
2023-07-03 01:58:18 -0400

    Se agrega loader en siim group

--- siim-group/src/app/providers.tsx
+++ siim-group/src/app/providers.tsx
@@ -1,13 +1,29 @@
 "use client";
 
+import { useIsMounted } from "@/hooks/useIsMounted";
 import { theme } from "@/theme/theme";
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
+import Loading from "./loading";
+import { useEffect, useState } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const [isMounted, setIsMounted] = useState(false);
+  const [showLoader, setShowLoader] = useState(true);
+
+  useEffect(() => {
+    setIsMounted(true);
+    setTimeout(() => {
+      setShowLoader(false);
+    }, 1000);
+  }, []);
+
   return (
     <CacheProvider>
-      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+      <ChakraProvider theme={theme}>
+        {showLoader && <Loading />}
+        {isMounted && children}
+      </ChakraProvider>
     </CacheProvider>
   );
 }
